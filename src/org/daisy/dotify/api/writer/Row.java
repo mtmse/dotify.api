aff42e4979d0d273c009ce28d605496d930fc502
Daniel Persson <mailto.woden@gmail.com>
2020-05-08 13:28:31 +0200

    Added external reference to the row for PEF output.

--- src/org/daisy/dotify/api/writer/Row.java
+++ src/org/daisy/dotify/api/writer/Row.java
@@ -18,4 +18,15 @@ public interface Row {
 	 * @return the row spacing, or null if not set
 	 */
 	public Float getRowSpacing();
+
+    /**
+     * This is an object that will be sent through the Dotify framework from the
+     * external reference tag in the OBFL specification and then used in the writer
+     * to add information to the rows of the PEF.
+     * This extra reference is not a part of the official PEF specification and used
+     * when the output is not meant for print.
+     *
+     * @return External reference object
+     */
+	public Object getExternalReference();
 }
